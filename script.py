bdc4fd8f546be680062c1fe337d719527579c8da
JohnnyTRT <john.smeds@gmail.com>
2024-02-06 23:37:10 -0500

    Rotate face

--- script.py
+++ script.py
@@ -23,4 +23,23 @@ layerThree = [
     ["y", "o", "g"]]
 cube = [layerOne, layerTwo, layerThree]
 
-def moveF(bCube):
+def listShiftLeft(list):
+    list.append(list[0])
+    list.remove(list[0])
+    return list
+def listShiftRight(list):
+    originalList = tuple(list)
+    list.remove(list[2])
+    list.insert(0, originalList[2])
+    return list
+def edgeFlip(inpEdge):
+    newEdge = [inpEdge[1], inpEdge[0]]
+    return newEdge
+
+def moveF(originalCube):
+    virtualCube = [originalCube[2][2]]
+    return(virtualCube)
+
+print(cube)
+
+
